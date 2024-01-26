b7df3ea9275d10bc4955387253767d1f65f55b9d
nidhibhagwatkar <138383650+nidhibhagwatkar@users.noreply.github.com>
2024-01-26 17:42:39 +0530

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,66 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	private int defaultPasswordLength = 10;
+	private String companySuffix = "anycompany.com";
+	
+	// constructor to receive firstname n lastname
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("email created " + this.firstName + " " + this.lastName);
+		this.department = setDepartment();
+		System.out.println("Department : "+this.department);
+		this.password =randomPassword(defaultPasswordLength);
+		System.out.println("Your password is  "+this.password);
+		email = firstName.toLowerCase() + "."+lastName.toLowerCase() +"@"+department+"."+companySuffix;
+		System.out.println("Your email is: "+email);
+		
+	}
+	// ask for the department
+	private String setDepartment() {
+		System.out.println("enter the department codes\n1 for sales\n2 for development\n3 for accounting\n0 for none");
+		Scanner dept = new Scanner(System.in);
+		int deptChoice = dept.nextInt();
+		if(deptChoice == 1) {
+			return "sales";
+		}
+		else if(deptChoice == 2) {
+			return "development";
+		}
+		else if(deptChoice == 3) {
+			return "accounting";
+		}
+		else {
+			return "";
+		}
+	}
+	// call  or generate a random password
+	private String randomPassword(int length) {
+		StringBuilder password = new StringBuilder();
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+		//char[] pasword = new char[length];
+		for(int i =0; i < length; i++) {
+		int rdm = (int)(Math.random() * passwordSet.length());
+		//password[i]=passwordSet.charAt(rdm);
+		password.append(passwordSet.charAt(rdm));
+		}
+		//return new String(password);
+		return password.toString();
+	}
+	
+	
+	// set the mailbox capacity
+	
+	// set the alternate email
+	// change the password
+}
